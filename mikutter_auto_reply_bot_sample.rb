d102e00de4cb7eddbe5432eeb0dd8cd905f3c0cd
Hiyowa Kyobashi <khiyowa@clara.local>
2016-06-07 04:54:16 +0900

    ひよわ焼く、ひよわを焼く、などに対応

--- mikutter_auto_reply_bot_sample.rb
+++ mikutter_auto_reply_bot_sample.rb
@@ -15,7 +15,7 @@ Plugin.create(:mikutter_auto_reply_bot_sample) do
 
     on_appear do |ms|
         ms.each do |m|
-            if m.message.to_s =~ /ひよわ焼き/ and m[:created] > DEFINED_TIME and !m.retweet? and !m.user.is_me?
+            if m.message.to_s =~ /ひよわ.*焼|焼.*ひよわ|ひよわやき|Hiyowayaki/ and m[:created] > DEFINED_TIME and !m.retweet? and !m.user.is_me?
                 # select reply dic & get sample reply
                 if m.message.to_s =~ /OR/
                     reply = sql.sample
