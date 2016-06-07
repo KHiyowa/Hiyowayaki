2f0201ea182de86fc90e3c2f54d720e93f0fac0d
Hiyowa Kyobashi <khiyowa@clara.local>
2016-06-08 04:15:48 +0900

    飯テロ対応

--- mikutter_auto_reply_bot_sample.rb
+++ mikutter_auto_reply_bot_sample.rb
@@ -8,6 +8,7 @@ Plugin.create(:mikutter_auto_reply_bot_sample) do
     # load reply dictionaries
     begin
         default = YAML.load_file(File.join(__dir__, 'dic', 'default.yml'))
+        meshi = YAML.load_file(File.join(__dir__, 'dic', 'meshi.yml'))
         sql = YAML.load_file(File.join(__dir__, 'dic', 'sql.yml'))
         version = YAML.load_file(File.join(__dir__, 'dic', 'version.yml'))
     rescue LoadError
@@ -16,13 +17,15 @@ Plugin.create(:mikutter_auto_reply_bot_sample) do
 
     on_appear do |ms|
         ms.each do |m|
-            if m.message.to_s =~ /ひよわ.?焼|焼.*ひよわ|ひよわやき|Hiyowayaki|hiyowayaki|HIYOWAYAKI/ \
+            if m.message.to_s =~ /ひよわ.?焼|ヒィヨォワァヤァキ|ひよわやき|Hiyowayaki|hiyowayaki|HIYOWAYAKI|ひよわ飯/ \
                     and m[:created] > DEFINED_TIME and !m.retweet? and !m.user.is_me?
                 # select reply dic & get sample reply
                 if m.message.to_s =~ /OR/
                     reply = sql.sample
                 elsif m.message.to_s =~ /--version|-v/
                     reply = version.sample
+                elsif m.message.to_s =~ /飯|めし/
+                    reply = meshi.sample
                 else
                     reply = default.sample
                 end
